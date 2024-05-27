e0cbf555de3522c54f073a60d12fd62c4652aa67
Ana-Beatriz-Marinho <anabibimn@hotmail.com>
2024-05-27 17:49:13 -0300

    Semáforos binários em ISR

---
+++ Curso_FreeRTOS/Semaforos/xSemaphoreBinaryISR/main/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+#include "driver/gpio.h"
+
+#define LED_PIN     (GPIO_NUM_2)
+#define BUTTON_PIN  (GPIO_NUM_0)
+
+SemaphoreHandle_t xSemaforo;
+TaskHandle_t xTaskButtonHandle;
+
+void vTask_BLINK(void *pvParameters)
+{
+    bool led_status = false;
+    while(1)
+    {
+        gpio_set_level(LED_PIN, led_status);
+        led_status = !led_status;
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+void vTask_BUTTON(void *pvParameters)
+{
+    uint16_t contador = 0;
+
+    while(1)
+    {
+        xSemaphoreTake(xSemaforo, portMAX_DELAY);
+        printf("x: %d\n", contador++);
+    }
+}
+
+void ISR_BUTTON(void *pvParameters)
+{
+    BaseType_t xHighPriorityTaskWoken = pdTRUE;
+
+    xSemaphoreGiveFromISR(xSemaforo, &xHighPriorityTaskWoken);
+
+    if(xHighPriorityTaskWoken == pdTRUE)
+    {
+        portYIELD_FROM_ISR();
+    }
+}
+
+void app_main(void)
+{
+    gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    gpio_pad_select_gpio(BUTTON_PIN);
+    gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
+
+    gpio_set_intr_type(BUTTON_PIN, GPIO_INTR_NEGEDGE);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(BUTTON_PIN, ISR_BUTTON, NULL);
+
+    xSemaforo = xSemaphoreCreateBinary();
+
+
+    xTaskCreate(vTask_BLINK, "Task Blink", configMINIMAL_STACK_SIZE + 1024, NULL, 1, NULL);
+    xTaskCreate(vTask_BUTTON, "Task Button", configMINIMAL_STACK_SIZE + 1024, NULL, 3, &xTaskButtonHandle);
+}
