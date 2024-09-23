6d0ab2de096b6973db34d27f4194b82c089b2d03
Ana-Beatriz-Marinho <anabibimn@hotmail.com>
2024-09-23 16:13:08 -0300

    Atualizações do código Ultrassônico

--- i2s_acquisition/main/main.c
+++ i2s_acquisition/main/main.c
@@ -10,8 +10,8 @@ void app_main(void) {
 
     // Inicializa o cartão SD
     init_sd_card();
-
     ESP_LOGI(TAG, "Cartão SD Inicializado!");
+
 
     // Configurando o i2s para modo padrão
     i2s_setup(&i2s_config, &pin_config);
@@ -22,13 +22,13 @@ void app_main(void) {
     ESP_ERROR_CHECK(i2s_start(I2S_NUM));
     vTaskDelay(100);  // Atraso para permitir que as mudanças tenham efeito
 
-    i2s_setup(&i2s_config, &pin_config);
+    //i2s_setup(&i2s_config, &pin_config);
     
-    ESP_ERROR_CHECK(i2s_set_sample_rates(I2S_NUM, 44100));  
+    //ESP_ERROR_CHECK(i2s_set_sample_rates(I2S_NUM, 44100));  
 
     ESP_LOGI(TAG, "Mudando para modo ultrassônico...");
-    vTaskDelay(pdMS_TO_TICKS(500));
-    ESP_ERROR_CHECK(i2s_set_sample_rates(I2S_NUM, 200000));
+    vTaskDelay(100);
+    ESP_ERROR_CHECK(i2s_set_sample_rates(I2S_NUM, 4100));
 
     // Define o tempo de início da gravação
     recording_start_time = xTaskGetTickCount();
@@ -41,23 +41,23 @@ void app_main(void) {
 
 }
 
-void i2s_task(void *pvParameter) {
+void i2s_task(void *pvParameter ) {
     char data_buffer[DATA_BUFFER_SIZE_ULT];
     size_t bytes_read = 0;
-
+    FILE *file = fopen("/sdcard/audio.raw", "ab");
+    
     while (1) {
         // Verifica se já se passou o tempo de gravação
         if (xTaskGetTickCount() - recording_start_time >= recording_duration_ticks) {
             ESP_LOGI(TAG, "Recording stopped.");
+            fclose(file);
             vTaskSuspend(NULL);
         } else {
-            if (i2s_read(I2S_NUM, (void *)data_buffer, DATA_BUFFER_SIZE_STD, &bytes_read, portMAX_DELAY) == ESP_OK) {
+            if (i2s_read(I2S_NUM, (void *)data_buffer, DATA_BUFFER_SIZE_ULT, &bytes_read, portMAX_DELAY) == ESP_OK) {
                 // Grava os dados no cartão SD
-                FILE *file = fopen("/sdcard/audio.raw", "ab");
                 if (file != NULL) {
                     fwrite(data_buffer, bytes_read, 1, file);
-                    printf("%x %x %x %x %x %x %x\n", data_buffer[0], data_buffer[1], data_buffer[2], data_buffer[3], data_buffer[4], data_buffer[5], data_buffer[6]);
-                    fclose(file);
+                    //printf("%x %x %x %x %x %x %x\n", data_buffer[1000], data_buffer[2100], data_buffer[3200], data_buffer[4000], data_buffer[4055], data_buffer[5], data_buffer[6]);
                 }
             }
         }
@@ -71,9 +71,10 @@ void i2s_setup(i2s_config_t *i2s_config, i2s_pin_config_t *pin_config) {
         //.mode = I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM,
         .mode = I2S_MODE_MASTER | I2S_MODE_RX,
         .sample_rate = 8000,
-        .bits_per_sample = BIT_DEPTH_STD,
-        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+        //.bits_per_sample = BIT_DEPTH_STD,
+        .bits_per_sample = BIT_DEPTH_ULT,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = I2S_COMM_FORMAT_I2S_MSB,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
         .dma_buf_count = DMA_BUF_COUNT,
         .dma_buf_len = DMA_BUF_LEN_SMPL,
@@ -81,34 +82,13 @@ void i2s_setup(i2s_config_t *i2s_config, i2s_pin_config_t *pin_config) {
     };
 
     *pin_config = (i2s_pin_config_t) {
-        .bck_io_num = I2S_PIN_NO_CHANGE,
-        .ws_io_num = MIC_CLOCK_PIN,
+        //.bck_io_num = I2S_PIN_NO_CHANGE,
+        //.ws_io_num = MIC_CLOCK_PIN,
+        .bck_io_num = MIC_CLOCK_PIN,
+        .ws_io_num = I2S_PIN_NO_CHANGE,
         .data_out_num = I2S_PIN_NO_CHANGE,
         .data_in_num = MIC_DATA_PIN
     };
 
    
-}
-
-void i2s_setup_ult(i2s_config_t *i2s_config, i2s_pin_config_t *pin_config) {
-
-    *i2s_config = (i2s_config_t) {
-        .mode = I2S_MODE_MASTER | I2S_MODE_RX,
-        .sample_rate = 312500,
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = DMA_BUF_COUNT,
-        .dma_buf_len = DMA_BUF_LEN_SMPL,
-        .use_apll = I2S_CLK_APLL,
-    };
-
-    *pin_config = (i2s_pin_config_t) {
-        .bck_io_num = I2S_PIN_NO_CHANGE,
-        .ws_io_num = MIC_CLOCK_PIN,
-        .data_out_num = I2S_PIN_NO_CHANGE,
-        .data_in_num = MIC_DATA_PIN
-    };
-
 }
--- i2s_acquisition/main/sd_driver.h
+++ i2s_acquisition/main/sd_driver.h
@@ -1,20 +1,64 @@
-#ifndef SD_DRIVER_H
-#define SD_DRIVER_H
+/*
+ * Include section
+ * --------------------
+ * Importing all necessary libraries for the good maintenance of the code
+ *
+ *  C_POSIX_LIB_INCLUDED:         standard functionalities
+ *  ESP_MANAGEMENT_LIBS_INCLUDED: log messages, error codes and SD filesystem support
+ */
 
-#include "esp_err.h"
-#include "esp_log.h"
-#include "driver/sdspi_host.h"
-#include "driver/spi_common.h"
-#include "sdmmc_cmd.h"
-#include "esp_vfs_fat.h"
+#ifndef ESP_MANAGEMENT_LIBS_INCLUDED
+    #define ESP_MANAGEMENT_LIBS_INCLUDED
+    #include "esp_err.h" // error codes and helper functions
+    #include "esp_log.h" // logging library
+    #include "esp_vfs_fat.h" // FAT filesystem support
+#endif
 
-#define PIN_NUM_MISO   GPIO_NUM_19
-#define PIN_NUM_MOSI   GPIO_NUM_23
-#define PIN_NUM_CLK    GPIO_NUM_18
-#define PIN_NUM_CS     GPIO_NUM_22
-#define MOUNT_POINT    "/sdcard"
-#define SPI_DMA_CHAN   1
+#ifndef C_POSIX_LIB_INCLUDED
+    #define C_POSIX_LIB_INCLUDED
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <sys/unistd.h>
+    #include <sys/stat.h>
+#endif //C_POSIX_LIB_INCLUDED
 
-esp_err_t init_sd_card(void);
+#define SD_DRIVER_TAG "sd_driver"
+#define INIT_SPI_TAG   "init_spi"
+#define DEINIT_SPI_TAG "deinit_spi"
+#define INIT_SD_TAG    "init_sd"
+#define DEINIT_SD_TAG  "deinit_sd"
 
-#endif // SD_DRIVER_H
+// sd card
+#define MOUNT_POINT "/sdcard" // SD card mounting directory
+#define SPI_DMA_CHAN 1        // DMA channel to be used by the SPI peripheral
+
+// spi bus
+#ifndef USE_SPI_MODE
+    #define USE_SPI_MODE    // define SPI mode
+    #define PIN_NUM_MISO 19 // SDI - Serial Data In
+    #define PIN_NUM_MOSI 23 // SDO - Serial Data Out
+    #define PIN_NUM_CLK  18 // System clock
+    #define PIN_NUM_CS   22 // Chip select
+#endif
+
+#ifndef _SD_DRIVER_H_
+#define _SD_DRIVER_H_
+
+/*
+ * Function prototype section
+ * --------------------
+ * Initialize functions prototypes to later be defined
+ */
+
+char* merge_filename(const char *filename);
+FILE* open_file(const char *filename, char *mode);
+void close_file(FILE **file);
+void rename_file(const char *actualfname, const char *targetfname);
+int initialize_spi_bus(sdmmc_host_t* host);
+int deinitialize_spi_bus(sdmmc_host_t* host);
+int initialize_sd_card(sdmmc_host_t* host, sdmmc_card_t** card);
+int deinitialize_sd_card(sdmmc_card_t** card);
+
+
+#endif  // _SD_DRIVER_H_
